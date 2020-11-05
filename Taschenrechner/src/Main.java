7d1321f04f84b135e2269ad7f6c0ebff3a2a27f7
Milchtank <fabripiac@gmail.com>
2020-11-05 19:33:40 +0100

    Adding initial code

--- Taschenrechner/src/Main.java
+++ Taschenrechner/src/Main.java
@@ -7,6 +7,36 @@ public class Main {
 		System.out.println("Hello World");
 		System.out.println("Test");
 		System.out.println("Test2");
+		
+		
+		
+		public int addition() {
+			
+			
+			return null;
+		}
+		
+		public int Subtraktion() {
+			
+			
+			return null;
+			
+		}
+		
+		public int multiplizieren() {
+			
+			
+			return null;
+		}
+		
+		
+		public float division() {
+			
+			
+			
+			return null;			
+			
+		}
 	}
 
 }
